b4efceeb9b86d069e28822ac51f85a038fcd26a2
sotpotatis <albin@albins.website>
2023-01-05 20:47:12 +0100

    Update GitHub Actions

--- .github/workflows/upload_to_netlify.sh
+++ .github/workflows/upload_to_netlify.sh
@@ -1,7 +1,7 @@
 #upload_to_netlify.sh
 #Quite self-explanatory script name. Also see the
 #GitHub Action with the same name.
-echo "Starting upload action..."
+echo "Starting upload action, uploading to ${NETLIFY_SITE_ID}..."
 echo "Installing dependencies..."
 npm install
 echo "✅ Dependencies installed!"
@@ -9,7 +9,7 @@ echo "Starting build..."
 npx parcel build src/html/index.html
 echo "✅ Build completed!"
 echo "Uploading to Netlify..."
-npx netlify link --name=$NETLIFY_SITE_ID
-npx netlify deploy --dir=dist --auth=$NETLIFY_SITE_TOKEN
+npx netlify link --name="${NETLIFY_SITE_ID}"
+npx netlify deploy --dir=dist --auth="${NETLIFY_SITE_TOKEN}"
 echo "✅ Uploaded to Netlify!"
 echo "🚀 Website successfully deployed to Netlify."
